2eed0859ffec946b5f273762ce43cc0aa980e781
karlin <karlin@atomicobject.com>
2010-03-03 14:17:20 -0500

    updated install and manpage for bash vers.

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ fi
 
 mkdir $HOME/.wd
 cp -i ./wd/* $HOME/.wd
-cp ./readme.txt $HOME/.wd
+cp ./README $HOME/.wd
 if [ -d "$HOME/.wd" ]; then
   echo Done. Please add the following lines to your .bashrc file:
 	echo
 	echo export WDHOME=\$HOME/.wd
-	echo source \$WDHOME/wdaliases.sh
+	echo source \$WDHOME/wd.sh
 	echo
 	echo Please copy wd.1.gz into your man page directory if you want to use it.
 	echo
